bb213d7dbb1c30c8652f593c1df36fdf22730468
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-03-14 04:00:36 +0000

    change from browser router to hash router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,35 @@
-import {Route, Routes} from "react-router-dom"
-import Layout from "./components/Layout"
-import FoodList from "./components/Display/FoodList"
-import RequiredAuth from "./components/Forms/Auth/RequiredAuth"
-import AddFoodForm from "./components/Forms/Food/AddFoodForm"
-import Login from "./components/Forms/Auth/Login"
-import Register from "./components/Forms/Auth/Register"
-import FoodDetails from "./components/Display/FoodDetails"
-import EditFood from "./components/Display/EditFood"
-import Hero from "./components/Hero"
-import Profile from "./components/Forms/Auth/Profile"
-
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import FoodList from "./components/Display/FoodList";
+import RequiredAuth from "./components/Forms/Auth/RequiredAuth";
+import AddFoodForm from "./components/Forms/Food/AddFoodForm";
+import Login from "./components/Forms/Auth/Login";
+import Register from "./components/Forms/Auth/Register";
+import FoodDetails from "./components/Display/FoodDetails";
+import EditFood from "./components/Display/EditFood";
+import Hero from "./components/Hero";
+import Profile from "./components/Forms/Auth/Profile";
 
 export default function App() {
   return (
     <Routes>
-      <Route  element={<Layout />} >
-        <Route index element={<Hero/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
+      <Route element={<Layout />}>
+        {/* Public Routes */}
+        <Route index element={<Hero />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-        <Route  element={<RequiredAuth/>}>
-          <Route path='/add_food' element={<AddFoodForm/>}/>
-          <Route path='/profile' element={<Profile/>}/>
+        {/* Protected Routes */}
+        <Route element={<RequiredAuth />}>
+          <Route path="/add_food" element={<AddFoodForm />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/food/:id" element={<FoodDetails />} />
+          <Route path="/food/edit/:id" element={<EditFood />} />
         </Route>
-        <Route path="food">
-          <Route index element={<FoodList/>}/>
-          <Route  element={<RequiredAuth/>}>
-            <Route path=':id' element={<FoodDetails/>}/>
-            <Route path="edit/:id" element={<EditFood/>}/>
-           </Route>
-        </Route>
-        
+
+        {/* Food List (Public) */}
+        <Route path="/food" element={<FoodList />} />
       </Route>
     </Routes>
-  )
+  );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {  HashRouter} from "react-router-dom"
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
 import { getAllFood } from './store/features/food/foodSlice.ts'
@@ -18,11 +18,9 @@ store.dispatch(getTotalVote())
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
+      <HashRouter>
+        <App/>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 )
