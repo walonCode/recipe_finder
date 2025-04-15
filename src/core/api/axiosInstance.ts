d8fe304bf000c8e9b3eca503a4892e957e2cc682
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-04-15 23:29:56 +0000

    change base url in axios

--- src/core/api/axiosInstance.ts
+++ src/core/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000/api/" : "https://recipe-finderserver.vercel.app/api/v1";
+const baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000/api/" : "https://recipe-finder-five-liart.vercel.app/api";
 
 export const axiosInstance = axios.create({
     baseURL,
